bee67864ef262bbabade8e3c72da1203b46a0378
István Nagy <nistvan.86@gmail.com>
2018-11-02 01:13:31 +0100

    Accept core path from command line; get fpga initialization status

--- src/fpga_io.rs
+++ src/fpga_io.rs
@@ -22,6 +22,8 @@ const FPGA_REG_BASE: u64 = 0xFF000000;
 const FPGA_REG_SIZE: libc::size_t = 0x01000000;
 
 const SOCFPGA_MGR_ADDRESS: u32 = 0xFF706000;
+const SOCFPGA_MGR_GPO_REGISTER_ADDRESS : u32 = SOCFPGA_MGR_ADDRESS + 0x10;
+const SOCFPGA_MGR_GPI_REGISTER_ADDRESS : u32 = SOCFPGA_MGR_ADDRESS + 0x14;
 
 /// Converts u32 to u8 little endian byte array
 #[inline]
@@ -32,6 +34,15 @@ fn u32tou8ale(v: u32) -> [u8; 4] {
         (v >> 16) as u8,
         (v >> 24) as u8,
     ]
+}
+
+/// Converts an u8 little endian byte array to u32
+#[inline]
+fn u8aletou32(a: [u8; 4]) -> u32 {
+    a[0] as u32 
+        + (a[1] as u32).rotate_left(8) 
+        + (a[2] as u32).rotate_left(16)
+        + (a[3] as u32).rotate_left(24)
 }
 
 impl FPGA {
@@ -47,6 +58,16 @@ impl FPGA {
 
         return match fpga.fpga_gpo_write(0) {
             Ok(_) => Ok(fpga),
+            Err(e) => Err(FPGAError::Io(e))
+        };
+    }
+
+    /// Checks whetever the FPGA core is ready.
+    /// This is done by checking the first bit of the GPI register which should be 1.
+    /// First bit is set to 1 once the core is started running. (TODO: is this correct?)
+    pub fn is_fpga_ready(&mut self) -> FPGAResult<bool> {
+        return match self.fpga_gpi_read() {
+            Ok(value) => Ok(value as i32 >= 0),
             Err(e) => Err(FPGAError::Io(e))
         };
     }
@@ -75,18 +96,38 @@ impl FPGA {
         };
     }
 
-    /// Writes a u32 to the FPGA manager's GPO register
+    /// Writes a new value to the FPGA manager's GPO register
     fn fpga_gpo_write(&mut self, value: u32) -> std::io::Result<()> {
-        self.writeu32le(value, SOCFPGA_MGR_ADDRESS + 0x10)?;
+        self.writeu32le(value, SOCFPGA_MGR_GPO_REGISTER_ADDRESS)?;
         Ok(())
+    }
+
+    // Reads the FPGA manager's GPI register
+    fn fpga_gpi_read(&mut self) -> std::io::Result<u32> {
+        self.readu32le(SOCFPGA_MGR_GPI_REGISTER_ADDRESS)
+    }
+
+    // Creates a mutable memory slice from the FPGA memory region for access
+    #[inline]
+    fn get_memory_slice(&mut self, offset: u32, size: u32) -> &mut [u8] {
+        let reg24 = (offset & 0xFFFFFF) >> 2;
+        &mut self.mem_map[reg24 as usize..(reg24 + size) as usize]
     }
 
     /// Writes a u32 to an FPGA memory position as little endian value
     #[inline]
     fn writeu32le(&mut self, value: u32, offset: u32) -> std::io::Result<()> {
-        let reg24 = (offset & 0xFFFFFF) >> 2;
-        (&mut self.mem_map[reg24 as usize..(reg24+3) as usize])
-            .write_all(&u32tou8ale(value)[..])?;
+        let mut slice = self.get_memory_slice(offset, 3);
+        slice.write_all(&u32tou8ale(value)[..])?;
         Ok(())
+    }
+
+    // Reads an FPGA memory position as little endian value into an u32 integer
+    #[inline]
+    fn readu32le(&mut self, offset: u32) -> std::io::Result<u32> {
+        let slice = self.get_memory_slice(offset, 3);
+        let mut u8lea : [u8; 4] = Default::default();
+        u8lea.copy_from_slice(&slice[..]);
+        Ok(u8aletou32(u8lea))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,36 @@ extern crate memmap;
 mod fpga_io;
 
 use std::process;
+use std::env;
 
 fn main() {
-    let _fpga = match fpga_io::FPGA::init() {
+    let mut fpga = match fpga_io::FPGA::init() {
         Ok(fpga) => fpga,
         Err(err) => {
             eprintln!("Failed to initialize FPGA: {:?}", err);
-            process::exit(1);
+            process::exit(0);
         }
     };
     
-    println!("Hello, world!");
+    println!("main_mister_rs starting.");
+
+    let args: Vec<String> = env::args().collect();
+
+    let default_core_path = &String::from("");
+    let core_path = args.get(0).or(Some(default_core_path)).unwrap();
+
+    println!("Core path: '{0}'", core_path);
+
+    match fpga.is_fpga_ready() {
+        Ok(ready) => {
+            if !ready {
+                eprintln!("FPGA is unitialized or incompatible core loaded.");
+                process::exit(0);
+            }
+        },
+        Err(err) => {
+            eprintln!("Failed to read FPGA ready state: {:?}", err);
+            process::exit(0);
+        }
+    }
 }
